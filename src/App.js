54974d9ea56d40f5984dea6f3abfad76240217c2
cheat2001 <chansocheatsok2001@gmail.com>
2022-08-15 14:31:30 +0700

    finished sliding components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Banner from './components/Banner'
 import "./App.css";
 import Navbar from './components/Navbar';
+import Sliding from './components/Sliding';
 
 function App() {
   return (
     <div>
       <Banner/>
       <Navbar/>
-     
+      <Sliding/>
     </div>
   )
 }
---
+++ src/components/Sliding.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import styles from "../styles/Sliding.module.css";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import slide1 from "../images/slide1.jpg";
+import slide2 from "../images/slide2.jpg";
+import slide3 from "../images/slide3.jpg";
+import slide4 from "../images/slide4.jpg";
+import slide5 from "../images/slide5.jpg";
+import slide6 from "../images/slide6.jpg";
+import slide7 from "../images/slide7.jpg";
+function Sliding() {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000
+  };
+  return (
+    <div className={styles.Container}>
+      <Slider {...settings}>
+          <div>
+            <img src={slide1} />
+          </div>
+          <div>
+          <img src={slide2} />
+          </div>
+          <div>
+          <img src={slide3} />
+          </div>
+          <div>
+          <img src={slide4} />
+          </div>
+          <div>
+          <img src={slide5} />
+          </div>
+          <div>
+          <img src={slide6} />
+          </div>
+          <div>
+          <img src={slide7} />
+          </div>
+        </Slider>
+    </div>
+  )
+}
+
+export default Sliding
